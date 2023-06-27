3479908ead1d35782810d93a5da64ce4f36fcaeb
jakub-bednarek <jakub.bednarek1009@gmail.com>
2023-06-27 22:49:38 +0200

    --logs command creating files

--- skrypt.sh
+++ skrypt.sh
@@ -1,5 +1,17 @@
 #! /bin/bash
 
-if [ $1 == "--date" ]; then
-    date
-fi
+create_log_files() {
+    for i in {1..100}
+    do
+        echo "${i} $(date) skrypt.sh" > logs$i.txt
+    done
+}
+
+for var in "$@"
+do
+    if [ $var == "--date" ]; then
+        date
+    elif [ $var == "--logs" ]; then
+        create_log_files
+    fi
+done
