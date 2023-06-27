3f50898f22a83843409419d7f70f8c42ae0b35f3
jakub-bednarek <jakub.bednarek1009@gmail.com>
2023-06-27 23:27:22 +0200

    single dash flags, advanced getopt

--- skrypt.sh
+++ skrypt.sh
@@ -1,5 +1,10 @@
 #! /bin/bash
 
+set -o errexit -o pipefail
+
+LONGOPTS=help,date,logs:
+OPTIONS=hdl:
+
 create_log_files() {
     if [ -z "$1" ] || [ $1 -le 0 ]; then
         echo "Invalid argument for --logs [-logs n_of_files]"
@@ -12,18 +17,35 @@ create_log_files() {
     done
 }
 
-for (( i=1; i<=$#; i++))
+! PARSED=$(getopt --options=$OPTIONS --longoptions=$LONGOPTS --name "$0" -- "$@")
+if [[ ${PIPESTATUS[0]} -ne 0 ]]; then
+    exit 1
+fi
+
+eval set -- "$PARSED"
+
+while true
 do
-    if [ ${!i} == "--date" ]; then
-        date
-    elif [ ${!i} == "--logs" ]; then
-        number_of_logs=$((i+1))
-        create_log_files ${!number_of_logs}
-    elif [ ${!i} == "--help" ]; then
-        echo "Syntax: skrypt.sh [--help|--date|--logs]"
-        echo "Available options:"
-        echo "--help                 Print helps"
-        echo "--date                 Print current date"
-        echo "--logs number_of_files Create number_of_files log files with index, date and script used"
-    fi
+    case "$1" in
+        -h|--help)
+            echo "Syntax: skrypt.sh [--help|--date|--logs]"
+            echo "Available options:"
+            echo "--help                 Print helps"
+            echo "--date                 Print current date"
+            echo "--logs number_of_files Create number_of_files log files with index, date and script use"
+            shift
+            ;;
+        -d|--date)
+            date
+            shift
+            ;;
+        -l|--logs)
+            create_log_files $2
+            shift 2
+            ;;
+        --)
+            shift
+            break
+            ;;
+    esac
 done
