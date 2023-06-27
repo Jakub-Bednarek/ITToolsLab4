647df59d22097c1a51b2a392e01ff035b2dc563b
jakub-bednarek <jakub.bednarek1009@gmail.com>
2023-06-27 23:01:04 +0200

    number of files to create for --logs

--- skrypt.sh
+++ skrypt.sh
@@ -1,17 +1,23 @@
 #! /bin/bash
 
 create_log_files() {
-    for i in {1..100}
+    if [ -z "$1" ] || [ $1 -le 0 ]; then
+        echo "Invalid argument for --logs [-logs n_of_files]"
+        return
+    fi
+
+    for (( i=1; i<=$1; i++))
     do
         echo "${i} $(date) skrypt.sh" > logs$i.txt
     done
 }
 
-for var in "$@"
+for (( i=1; i<=$#; i++))
 do
-    if [ $var == "--date" ]; then
+    if [ ${!i} == "--date" ]; then
         date
-    elif [ $var == "--logs" ]; then
-        create_log_files
+    elif [ ${!i} == "--logs" ]; then
+        number_of_logs=$((i+1))
+        create_log_files ${!number_of_logs}
     fi
 done
